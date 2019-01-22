4f417b49e4c481cd613174c6ee9a85114bcc055f
ielgned <ielgned@outlook.com>
2019-01-22 18:18:35 +0800

    修改

---
+++ assets/Script/Original/UI.js
@@ -0,0 +1,18 @@
+/**
+ * @author uu
+ * @file UI刷新
+ * @todo 
+ */
+cc.Class({
+  extends: cc.Component,
+  properties: {},
+  start() {
+
+  },
+  init(g) {
+    this._game = g
+  },
+  lateInit() {
+
+  },
+});
